036d4551efa64bc6fd4c3bdf0841450f3505c878
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-09-23 18:09:40 -0500

    Dependencia

---
+++ SystemHomeEnergy.IOC/Dependencia.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemHomeEnergy.MODELS;
+
+namespace SystemHomeEnergy.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencias(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BdhomeEnergyContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+            });
+            //en la siguiente linea utilizamos un modelo generico
+        }
+    }
+}
