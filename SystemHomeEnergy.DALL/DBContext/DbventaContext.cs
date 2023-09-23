96fdbb831a3325097b35d2ef7eb811a405c0e472
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-09-23 14:30:20 -0500

    Agregar archivos de proyecto.

---
+++ SystemHomeEnergy.DALL/DBContext/DbventaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using SystemHomeEnergy;
+
+
+namespace SystemHomeEnergy.DALL.DBContext
+{
+    public partial class DbventaContext : DbContext
+    {
+        public DbventaContext()
+        {
+        }
+    }
+}
