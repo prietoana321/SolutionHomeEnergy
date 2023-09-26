1b654738736e66d071937b6ec1660fc67963decb
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-09-26 11:40:54 -0500

    Si metodo

--- SystemHomeEnergy.DALL/Repositorios/VentaRepository.cs
+++ SystemHomeEnergy.DALL/Repositorios/VentaRepository.cs
@@ -19,9 +19,50 @@ namespace SystemHomeEnergy.DALL.Repositorios
             _dbContext = dbContext;
         }
 
-        public Task<Cotizacion> Registrar(Cotizacion modelo)
+        public async Task<Cotizacion> Registrar(Cotizacion modelo)
         {
-            throw new NotImplementedException();
+            //CREAMOS UNA VARIABLE
+            Cotizacion cotizacionGenerada = new();
+            //si dentro de la logica ocurre un error la linea siguiente tiene que reestablecer todo al principio
+            using (var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach (Cotizacion dv in modelo.Contrato)
+                    {
+                        Servicio servicio_encontrado = _dbContext.Servicios.Where(p => p.IdServicio == dv.IdCotizacion).First();
+                        _dbContext.Servicios.Update(servicio_encontrado);
+                    }
+                    await _dbContext.SaveChangesAsync();
+                    NumeroDocumento correlativo = _dbContext.NumeroDocumentos.First();
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+                    _dbContext.NumeroDocumentos.Update(correlativo);
+                    await _dbContext.SaveChangesAsync();
+                    //0001
+                    int CantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", CantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - CantidadDigitos, CantidadDigitos);
+                    modelo.NumeroDocumento = numeroVenta;
+
+                    await _dbContext.AddAsync(modelo);
+                    await _dbContext.SaveChangesAsync();
+
+                    cotizacionGenerada = modelo;
+                    //la transaccion puede finalizar sin nigun problema
+                    transaction.Commit();
+                }
+                catch
+                {
+                    //devolvera todo como estaba antes
+                    transaction.Rollback();
+                    //devuelve el error
+                    throw;
+                }
+                return cotizacionGenerada;
+            }
+            //  throw new NotImplementedException();
         }
     }
 }
