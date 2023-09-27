bfc297fdd00c3e1d6db13d89ec7a7a4d8569e3b1
angiecasta12141997 <angieconcordia@gmail.com>
2023-09-27 13:20:30 -0500

    creacion auditor dto

---
+++ SystemHomeEnergy.DTO/AuditorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    public class AuditorDTO
+    {
+        public int IdUsuario { get; set; }
+
+        public string? NombreCompleto { get; set; }
+    }
+}
