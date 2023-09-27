ef10955f4a82a23f4c4803c08510b6d21c2991db
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-09-26 22:29:44 -0500

    modelo cotizacionsemana

---
+++ SystemHomeEnergy.DTO/CotizacionSemanaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    public class CotizacionSemanaDTO
+    {
+        public string? FechaContrato { get; set; }
+        public int? TotalSemana { get; set; }
+
+    }
+}
