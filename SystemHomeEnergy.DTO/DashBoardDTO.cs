661fbae6d4e86079ad2dbba7881c9ccf8ad85406
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-09-26 22:38:28 -0500

    modelo dashboarddto

---
+++ SystemHomeEnergy.DTO/DashBoardDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    internal class DashBoardDTO
+    {
+        public int? TotalContratos { get; set; }
+        public string? TotalIngresos { get; set; }
+        public List<CotizacionSemanaDTO>CotizacionUltimaSemana { get; set; }
+    }
+}
