a348ae08665325b010d29ad3e447021a2c262647
angiecasta12141997 <angieconcordia@gmail.com>
2023-09-27 12:29:47 -0500

    creacion procesos dto

---
+++ SystemHomeEnergy.DTO/ProcesoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    public class ProcesoDTO
+    {
+        public int IdEstado { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Descripcion { get; set; }
+    }
+}
