315e0b704a16076e2a9c30ee6166abb552394045
angiecasta12141997 <angieconcordia@gmail.com>
2023-09-27 14:04:20 -0500

    creacion razon dto

---
+++ SystemHomeEnergy.DTO/RazonDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    public class RazonDTO
+    {
+        public int IdCotizacion { get; set; }
+        public string Razon { get; set; }
+        public int IdEstado { get; set; }
+    }
+}
