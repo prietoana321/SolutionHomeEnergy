2d9578572d57b418c9bb05b5ff2886666c1484cb
angiecasta12141997 <angieconcordia@gmail.com>
2023-09-27 15:47:57 -0500

    creacion automapper

---
+++ SystemHomeEnergy.UTILITY/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SystemHomeEnergy.DTO;
+using SystemHomeEnergy.MODELS;
+
+namespace SystemHomeEnergy.UTILITY
+{
+    public class AutoMapperProfile: Profile
+    {
+    }
+}
