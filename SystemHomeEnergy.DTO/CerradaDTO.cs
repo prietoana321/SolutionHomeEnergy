798d3a69d342f0210726a8ba382d7ea281a947a2
angiecasta12141997 <angieconcordia@gmail.com>
2023-09-27 12:50:40 -0500

    creacion cerrados dto

---
+++ SystemHomeEnergy.DTO/CerradaDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    public class CerradaDTO
+    {
+        public int IdCotizacion { get; set; }
+
+        public string? NumeroDocumento { get; set; }
+
+        public string? FormaPago { get; set; }
+
+        public int? Plazo { get; set; }
+
+        public double? TotalInteres { get; set; }
+
+        public double? Totalinteresxmes { get; set; }
+
+        public double? Ahorra { get; set; }
+
+        public double? Consumoelec { get; set; }
+
+        public double? Consumosolar { get; set; }
+
+        public decimal? Total { get; set; }
+
+        public string? Socein { get; set; }
+
+        public string? Tamañosistema { get; set; }
+
+        public double? Inicial { get; set; }
+
+        public string? Notas { get; set; }
+
+        public string? Recursos { get; set; }
+
+        public string? Url { get; set; }
+
+        public int? IdEstado1 { get; set; }
+
+        public bool? EsActivo { get; set; }
+
+        public int? Plazoconsumo { get; set; }
+        public DateTime? Fecha { get; set; }
+    }
+}
---
+++ SystemHomeEnergy.DTO/ProspectoDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHomeEnergy.DTO
+{
+    public class ProspectoDTO
+    {
+        public int IdProspecto { get; set; }
+
+        public string? Fachadaimg { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? NombreCompleto { get; set; }
+
+        public string? Direccion { get; set; }
+
+        public string? Contacto { get; set; }
+
+        public string? RazonSocial { get; set; }
+
+        public int? Idauditor { get; set; }
+
+        public string? Detalle { get; set; }
+
+        public bool? EsActivo { get; set; }
+
+        public DateTime? Fecha { get; set; }
+    }
+}
