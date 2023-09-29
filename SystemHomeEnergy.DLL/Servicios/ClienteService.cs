1e6534352cd44db86bf848881b7935b0e1cd656c
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-09-29 08:04:43 -0500

    cliente service

---
+++ SystemHomeEnergy.DLL/Servicios/ClienteService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemHomeEnergy.DALL.Repositorios.Contrato;
+using SystemHomeEnergy.DLL.Servicios.Contrato;
+using SystemHomeEnergy.DTO;
+using SystemHomeEnergy.MODELS;
+
+namespace SystemHomeEnergy.DLL.Servicios
+{
+    public   class ClienteService
+    {
+        private readonly IGenericRepository<Usuario> _usuarioRepositorio;
+
+        private readonly IMapper _mapper;
+    }
+}
---
+++ SystemHomeEnergy.DLL/Servicios/Contrato/IClienteService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemHomeEnergy.DTO;
+
+namespace SystemHomeEnergy.DLL.Servicios.Contrato
+{
+    public interface IClienteService
+    {
+        Task<List<ClienteDTO>> lista();
+
+        Task<ClienteDTO> Crear(ClienteDTO modelo);
+        Task<bool> Editar(ClienteDTO modelo);
+        Task<bool> Eliminar(int Id);
+    }
+}
