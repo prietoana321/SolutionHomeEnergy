8c8515bc9de6677b7c75cb02773e90af4c86676b
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2023-10-01 13:01:56 -0500

    mIS NLDNK

---
+++ SystemHomeEnergy.API/Utilidad/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemHomeEnergy.API.Utilidad
+{
+    public class Response<T>
+    {
+        public bool Status { get; set; }
+
+        public T Value { get; set; }
+        public string msg { get; set; }
+    }
+}
