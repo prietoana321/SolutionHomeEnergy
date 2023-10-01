58fd8cedeae2d6405132f7ce66da465039f286ea
angiecasta12141997 <angieconcordia@gmail.com>
2023-10-01 12:59:00 -0500

    M,IS CAMGBIGSYHGBJYHCD

--- SystemHomeEnergy.DLL/Servicios/ProspectoService.cs
+++ SystemHomeEnergy.DLL/Servicios/ProspectoService.cs
@@ -22,20 +22,6 @@ namespace SystemHomeEnergy.DLL.Servicios
             _prospectoRepositorio = prospectoRepositorio;
             _mapper = mapper;
         }
-<<<<<<< HEAD
-
-        public async Task<ProspectoDTO> Crear(ProspectoDTO modelo)
-        {
-            try
-            {
-                var ProspectoCreado = await _prospectoRepositorio.Crear(_mapper.Map<Prospecto>(modelo));
-                if (ProspectoCreado.IdProspecto == 0)
-                {
-                    throw new TaskCanceledException(" no se pudo crear");
-                }
-                var query = await _prospectoRepositorio.Consultar(u => u.IdProspecto == ProspectoCreado.IdProspecto);
-                ProspectoCreado = query.Include(Prospecto => Prospecto.IdProspecto).First();
-=======
         public async Task<List<ProspectoDTO>> Lista()
         {
             try
@@ -60,45 +46,18 @@ namespace SystemHomeEnergy.DLL.Servicios
                 //con el await obtenemos, si es cero el prospecto fallo, sino, continuaria aquí
                 var query = await _prospectoRepositorio.Consultar(u => u.IdProspecto == ProspectoCreado.IdProspecto);
                 ProspectoCreado = query.Include(Auditor => Auditor.Idauditor).First();
->>>>>>> 5cde3ad063724c937d46599d79734f4e932c5d2d
                 return _mapper.Map<ProspectoDTO>(ProspectoCreado);
             }
             catch
             {
                 throw;
             }
-<<<<<<< HEAD
-
-=======
->>>>>>> 5cde3ad063724c937d46599d79734f4e932c5d2d
         }
 
         public async Task<bool> Editar(ProspectoDTO modelo)
         {
             try
             {
-<<<<<<< HEAD
-                var ProspectoModelo = _mapper.Map<Prospecto>(modelo);
-                var ProspectoEncontrado = await _prospectoRepositorio.Obtener(u => u.IdProspecto == ProspectoModelo.IdProspecto);
-                if (ProspectoEncontrado == null)
-                {
-                    throw new TaskCanceledException("No se encuentra el Prospecto");
-                }
-                ProspectoEncontrado.Contacto = ProspectoModelo.Contacto;
-                ProspectoEncontrado.Idauditor = ProspectoEncontrado.Idauditor;
-                ProspectoEncontrado.RazonSocial = ProspectoEncontrado.RazonSocial;
-                ProspectoEncontrado.Direccion = ProspectoEncontrado.Direccion;
-                ProspectoEncontrado.Fecha = ProspectoEncontrado.Fecha;
-                ProspectoEncontrado.EsActivo = ProspectoModelo.EsActivo;
-
-                bool respuesta = await _prospectoRepositorio.Editar(ProspectoEncontrado);
-                if (respuesta == false)
-                {
-                    throw new TaskCanceledException("No se puede Editar");
-                }
-                return respuesta;
-
-=======
                 var prospectoModelo = _mapper.Map<Prospecto>(modelo);
                 var prospectoEncontrado = await _prospectoRepositorio.Obtener(u => u.IdProspecto == prospectoModelo.IdProspecto);
                 if (prospectoEncontrado == null)
@@ -116,14 +75,13 @@ namespace SystemHomeEnergy.DLL.Servicios
                 prospectoEncontrado.Detalle = prospectoEncontrado.Detalle;
                 prospectoEncontrado.EsActivo = prospectoEncontrado.EsActivo;
 
-                 bool respuesta = await _prospectoRepositorio.Editar(prospectoEncontrado);
+                bool respuesta = await _prospectoRepositorio.Editar(prospectoEncontrado);
 
                 if (respuesta == false)
                 {
                     throw new TaskCanceledException("No se pudo editar");
                 }
                 return respuesta;
->>>>>>> 5cde3ad063724c937d46599d79734f4e932c5d2d
             }
             catch
             {
@@ -135,17 +93,6 @@ namespace SystemHomeEnergy.DLL.Servicios
         {
             try
             {
-<<<<<<< HEAD
-                var ProspectoEncontrado = await _prospectoRepositorio.Obtener(u => u.IdProspecto == Id);
-                if (ProspectoEncontrado == null)
-                {
-                    throw new TaskCanceledException("El Prospecto no exiate");
-                }
-                bool respuesta = await _prospectoRepositorio.Eliminar(ProspectoEncontrado);
-                if (respuesta == false)
-                {
-                    throw new TaskCanceledException("No se elimino con exito");
-=======
                 var prospectoEncontrado = await _prospectoRepositorio.Obtener(v => v.IdProspecto == Id);
                 if (prospectoEncontrado == null)
                 {
@@ -155,7 +102,6 @@ namespace SystemHomeEnergy.DLL.Servicios
                 if (respuesta == false)
                 {
                     throw new TaskCanceledException("No se pudo eliminar");
->>>>>>> 5cde3ad063724c937d46599d79734f4e932c5d2d
                 }
                 return respuesta;
             }
@@ -164,23 +110,6 @@ namespace SystemHomeEnergy.DLL.Servicios
                 throw;
             }
         }
-
-<<<<<<< HEAD
-        public async Task<List<Prospecto>> lista()
-        {
-            try
-            {
-                var queryProspecto = await _prospectoRepositorio.Consultar();
-                var listaProspecto = queryProspecto.Include(cat => cat.Idauditor).ToList();
-                return _mapper.Map<List<ProspectoDTO>>(listaProspecto).ToList();
-            }
-            catch
-            {
-                throw;
-            }
-        }
-=======
->>>>>>> 5cde3ad063724c937d46599d79734f4e932c5d2d
     }
 }
 
