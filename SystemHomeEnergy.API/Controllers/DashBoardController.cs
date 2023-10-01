37508284e859a078ddd74aecd268fb9b0aa34405
angiecasta12141997 <angieconcordia@gmail.com>
2023-10-01 18:47:07 -0500

    llenado exitoso de controlador dashboard

---
+++ SystemHomeEnergy.API/Controllers/DashBoardController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SystemHomeEnergy.DLL.Servicios.Contrato;
+using SystemHomeEnergy.DTO;
+using SystemHomeEnergy.MODELS;
+using SystemHomeEnergy.API.Utilidad;
+
+namespace SystemHomeEnergy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashBoardController : ControllerBase
+    {
+        private readonly IDashBoardService _dashBoardServicio;
+
+        public DashBoardController(IDashBoardService dashBoardServicio)
+        {
+            _dashBoardServicio = dashBoardServicio;
+        }
+        public async Task<IActionResult>Resumen()
+        {
+            var rsp = new Response<DashBoardDTO>();
+            try
+            {
+                rsp.Status = true;
+                rsp.Value = await _dashBoardServicio.Resumen();
+            }
+            catch (Exception ex)
+            {
+                rsp.Status = false;
+                rsp.msg = ex.Message;
+            }
+            //TODAS LOS SOLICITUDES SERÁN RESPUESTAS EXITOSAS
+            return Ok(rsp);
+        }
+    }
+}
